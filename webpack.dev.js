28e7f6e3ab9d7b7f488d8dc1b7309933f3ae5f5f
ZakharDay <zakhar.day@gmail.com>
2022-05-30 13:43:14 +0300

    Build directory changed to docs

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './build'
+    contentBase: './dev_build'
   },
   watch: true,
   output: {
